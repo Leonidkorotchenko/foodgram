96e3bba15778f3079461e7e4d11c946929ed0c7a
Leonidkorotchenko <speedleo912@gmail.com>
2025-01-28 22:06:35 +0300

    Add Action

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -102,10 +102,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '../backend_static/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media/cats/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
